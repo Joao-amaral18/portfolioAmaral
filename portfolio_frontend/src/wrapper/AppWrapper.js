d28a14b074b0b08849fb8504e26347ebc1a0b8ea
Joao-amaral18 <joaovitor.lima.amaral@gmail.com>
2022-11-03 11:04:49 -0300

    app wrapper

--- portfolio_frontend/src/wrapper/AppWrapper.js
+++ portfolio_frontend/src/wrapper/AppWrapper.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 import { NavgationDots, SocialMedia } from '../components'
 
-function AppWrapper() {
+const AppWrapper = (Component, idName, ClassNames) => function HOC() {
     return (
-        <div>AppWrapper</div>
+        <div id={idName} className={`app__cotainer ${ClassNames}`}>
+            <SocialMedia />
+            <div className='app__wrapper app__flex'>
+                <Component />
+                <div className='copyright'>
+                    <p className='p-text'>@2022 JOAO</p>
+                    <p className='p-text'>All rights reserved</p>
+                </div>
+            </div>
+            <NavgationDots active={idName} />
+        </div>
     )
 }
 
